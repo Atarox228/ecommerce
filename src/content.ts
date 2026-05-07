395a011013cb17982ebc5ed3117309eb001bb883
Atarox <80927839+Atarox228@users.noreply.github.com>
2026-05-06 23:14:06 -0300

    cambios en numero

--- src/content.ts
+++ src/content.ts
@@ -110,7 +110,7 @@ export const content = {
       clearLabel: 'Vaciar carrito',
       checkoutLabel: 'Ir al carrito',
       payLabel: 'Pagar',
-      whatsappNumber: '1159045262',
+      whatsappNumber: '+1159045262',
       messageIntro: 'Hola, quiero hacer este pedido:',
       messageFooter: 'Quedo a la espera de la confirmación. Gracias.',
       quantityLabel: 'Cantidad',
